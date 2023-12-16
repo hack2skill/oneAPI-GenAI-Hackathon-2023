3cedd1ffefeb64956637b71a40cbb9d8fd88dc3c
bhaskarturkar <150421011+bhaskarturkar@users.noreply.github.com>
2023-12-16 07:00:16 +0530

    Update compare_doc.py final

--- pages/compare_doc.py
+++ pages/compare_doc.py
@@ -1,35 +1,31 @@
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain import HuggingFacePipeline
+# Import necessary libraries
 import sys
 import site
 from pathlib import Path
 import os
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain import HuggingFacePipeline
 import logging
-import os
 import random
 import re
 import warnings
 import torch
 import intel_extension_for_pytorch as ipex
 
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline # ,BitsAndBytesConfig
 from transformers import LlamaTokenizer, LlamaForCausalLM
-from transformers import BertTokenizer, BertForSequenceClassification
-from langchain import HuggingFacePipeline
+from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 
-
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import LLMChain
 from langchain.chains import RetrievalQA 
 from langchain.chains.question_answering import load_qa_chain
 from langchain.indexes import VectorstoreIndexCreator
 import base64
 import streamlit as st
-import os
-# import json
 from pathlib import Path
-# import ast
 
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.globals import set_debug
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -47,8 +43,9 @@ from src.entity import QAModel
 import shutil
 import logging
 from difflib import SequenceMatcher
-import re
-
+from langchain.globals import set_verbose, set_debug
+set_verbose(True)
+set_debug(True)
 
 
 # Suppress warnings for a cleaner output
@@ -57,6 +54,7 @@ warnings.filterwarnings("ignore")
 os.environ["SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS"] = "1"
 os.environ["ENABLE_SDP_FUSION"] = "1"
 
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 # Get Python version
 def get_python_version():
     return "python" + ".".join(map(str, sys.version_info[:2]))
@@ -128,134 +126,39 @@ def select_device(preferred_device=None):
 
 ###########################################################################################
 # Cache path for the model
-MODEL_CACHE_PATH = "./my_llama_model"
+MODEL_CACHE_PATH = "./" 
 
-class hf_model():
-    """
-    ChatBotModel is a class for generating responses based on text prompts using a pretrained model.
+# Model name to be used
+model_name = "perlthoughts/Falkor-11b"
 
-    Attributes:
-    - device: The device to run the model on. Default is "xpu" if available, otherwise "cpu".
-    - model: The loaded model for text generation.
-    - tokenizer: The loaded tokenizer for the model.
-    - torch_dtype: The data type to use in the model.
-    """
-
-    def __init__(
-        self,
-        model_id_or_path: str ="meta-llama/Llama-2-7b-chat-hf",#"meta-llama/Llama-2-13b-chat-hf",#"circulus/Llama-2-13b-orca-v1", #"my_llama_model",# "circulus/Llama-2-13b-orca-v1",#"microsoft/Orca-2-13b",#"01-ai/Yi-34B",#"bhenrym14/platypus-yi-34b",#"tiiuae/hf-7b-instruct",  #"Intel/neural-chat-7b-v3-1",  # "Writer/camel-5b-hf",  #tiiuae/hf-7b-instruct
-        torch_dtype: torch.dtype = torch.bfloat16,
-        optimize: bool = True,
-    ) -> None:
-        """
-        The initializer for ChatBotModel class.
-
-        Parameters:
-        - model_id_or_path: The identifier or path of the pretrained model.
-        - torch_dtype: The data type to use in the model. Default is torch.bfloat16.
-        - optimize: If True, ipex is used to optimized the model
-        """
-        self.torch_dtype = torch_dtype
-        self.device = select_device("xpu")
-        self.model_id_or_path = model_id_or_path
-        # local_model_id = self.model_id_or_path.replace("/", "--")
-        # model_id_or_path = os.path.join(MODEL_CACHE_PATH, local_model_id) #local_model_path = "meta-llama/Llama-2-13b-chat-hf"
-        # local_model_path = "my-model"
-        # model_id_or_path = "meta-llama/Llama-2-13b-chat-hf"
-        local_model_id = self.model_id_or_path.replace("/", "--")
-        local_model_path = os.path.join(MODEL_CACHE_PATH, local_model_id)
-        
-        if (
-            self.device == self.device.startswith("xpu")
-            if isinstance(self.device, str)
-            else self.device.type == "xpu"
-        ):
-
-            self.autocast = torch.xpu.amp.autocast
-        else:
-            self.autocast = torch.cpu.amp.autocast
-        self.torch_dtype = torch_dtype
-        try:
-            if "llama" in model_id_or_path:
-                self.tokenizer = LlamaTokenizer.from_pretrained(local_model_path,use_auth_token=token,cache_dir="my_llama_model/")
-                self.model = (
-                    LlamaForCausalLM.from_pretrained(
-                        local_model_path,use_auth_token=token,
-                        low_cpu_mem_usage=True,
-                        torch_dtype=self.torch_dtype,cache_dir="my_llama_model/"
-                    )
-                    .to(self.device)
-                    .eval()
-                )
-            else:
-                self.tokenizer = AutoTokenizer.from_pretrained(
-                    local_model_path, trust_remote_code=True,cache_dir="my_llama_model/"
-                )
-                self.model = (
-                    AutoModelForCausalLM.from_pretrained(
-                        local_model_path,
+model= AutoModelForCausalLM.from_pretrained(
+                        model_name,
                         low_cpu_mem_usage=True,
                         trust_remote_code=True,
-                        torch_dtype=self.torch_dtype,cache_dir="my_llama_model/"
-                    )
-                    .to(self.device)
-                    .eval()
-                )
-        except (OSError, ValueError, EnvironmentError) as e:
-            logging.info(
-                f"Tokenizer / model not found locally. Downloading tokenizer / model for {self.model_id_or_path} to cache...: {e}"
-            )
-            if "llama" in model_id_or_path:
-                self.tokenizer = LlamaTokenizer.from_pretrained(self.model_id_or_path,use_auth_token=token,cache_dir="my_llama_model/")
-                self.model = (
-                    LlamaForCausalLM.from_pretrained(
-                        self.model_id_or_path,
-                        low_cpu_mem_usage=True,use_auth_token=token,
-                        torch_dtype=self.torch_dtype,cache_dir="my_llama_model/"
-                    )
-                    .to(self.device)
-                    .eval()
-                )
-            else:
-                self.tokenizer = AutoTokenizer.from_pretrained(
-                    self.model_id_or_path, trust_remote_code=True,cache_dir="my_llama_model/"
-                )
-                self.model = (
-                    AutoModelForCausalLM.from_pretrained(
-                        self.model_id_or_path,
-                        low_cpu_mem_usage=True,
-                        trust_remote_code=True,
-                        torch_dtype=self.torch_dtype,cache_dir="my_llama_model/"
-                    )
-                    .to(self.device)
-                    .eval()
-                )
+                        torch_dtype=torch.bfloat16,cache_dir="./").to("cpu").eval()   #load_in_8bit=True
+
+# IPEX for Quantization and Optimization
+qconfig = ipex.quantization.get_weight_only_quant_qconfig_mapping(
+  weight_dtype=torch.qint8, # or torch.quint4x2
+  lowp_mode=ipex.quantization.WoqLowpMode.NONE, # or FP16, BF16, INT8
+)
+
+checkpoint = None # optionally load int4 or int8 checkpoint #"falkor-11b-q8_0.gguf" # 
+model = ipex.optimize_transformers(model, quantization_config=qconfig, low_precision_checkpoint=checkpoint)
+tokenizer = AutoTokenizer.from_pretrained(
+                    model_name, trust_remote_code=True,cache_dir="./"           )
 
 
-        
-        self.max_length = 256
+def generate_text():
+    gt = pipeline(task="text-generation", model=model, tokenizer=tokenizer)
 
-        if optimize:
-            if hasattr(ipex, "optimize_transformers"):
-                try:
-                    ipex.optimize_transformers(self.model, dtype=self.torch_dtype)
-                except:
-                    ipex.optimize(self.model, dtype=self.torch_dtype)
-            else:
-                ipex.optimize(self.model, dtype=self.torch_dtype)
+    return gt
 
 
-    def generate_text(self):
-        gt = pipeline(task="text-generation", model=self.model, tokenizer=self.tokenizer)
+hf_pipeline = HuggingFacePipeline(pipeline=generate_text())
 
-        return gt
-#################################################################
 
-# HuggingFacePipeline for the Model
-token = "hugging face token"
-# LangChain HuggingFacePipeline set to our transformer pipeline
-hf_model_obj=hf_model()
-hf_pipeline = HuggingFacePipeline(pipeline=hf_model_obj.generate_text())
+token = "your hugging face token"
 
 # Image for the Streamlit page
 img = Image.open("images/affine.jpg")
@@ -309,9 +212,6 @@ st.write("\n")
 path = "data/Contracts/"
 
 with st.sidebar:
-    # key=col1._text_input()
-    # Left column: Upload PDF text
-    # st.header("Dashboard")
     st.markdown("""
 <div style='text-align: center;top-margin:90px;'>
 <h2 style='font-size: 20px; font-family: Arial, sans-serif; 
@@ -335,7 +235,7 @@ with st.sidebar:
 
     print(subdirs)
     doc1 = st.selectbox('**Pick the First document :**', sorted(subdirs), key="0")
-
+    # doc1, doc2 = "leave1.pdf", "leave2.pdf"
     doc2 = st.selectbox('**Pick the Second document :**', sorted(subdirs), key="1")
     if doc1 !="Select" and doc2 !="Select": 
 
@@ -345,15 +245,12 @@ with st.sidebar:
         st.write("\n")
         st.write("\n")
 
-
-
 qa_model=QAModel()
-
 
 file_1 = f"{path}{doc1}"
 file_2 = f"{path}{doc2}"
 
-footer = """ """
+footer = """Analytics"""
 special_characters = r"[]{}()^$.*+?|\\"
 
     # Escape special characters by adding a backslash before them
@@ -378,9 +275,15 @@ if trigger_1 :
             documents1 += ' '.join(page.extract_text().splitlines())
                             # split into chunks
     documents1 = re.sub(pattern, replacement, documents1)
-    document_chunks1=qa_model.document_splitter_assistant(documents1,user_input_chunk_size = 2000,user_input_chunk_overlap = 300)  
-
-    vectorstore_doc1 = qa_model.create_embedding_assistant(document_chunks1)
+    document_chunks1=qa_model.document_splitter_assistant(documents1,user_input_chunk_size = 300,user_input_chunk_overlap = 30)  
+    embedd_path = os.path.join("./chroma_db",doc1.split(".")[0])
+    if not os.path.exists(embedd_path):
+        if not os.path.exists("./chroma_db"):
+            os.mkdir("./chroma_db")
+        os.mkdir(embedd_path) #+"/")
+        vectorstore_doc1 = qa_model.create_embedding_assistant(document_chunks1, embedd_path)
+    else:
+        vectorstore_doc1 = Chroma(persist_directory=embedd_path, embedding_function = embeddings)
     pdf_reader2=PyPDFLoader(file_2)
     if file_2 is not None:
         pdf_reader = PdfReader(file_2)
@@ -392,64 +295,80 @@ if trigger_1 :
                             # split into chunks
 
     documents2 = re.sub(pattern, replacement, documents2)
-    document_chunks2=qa_model.document_splitter_assistant(documents2,user_input_chunk_size = 2000,user_input_chunk_overlap = 300) 
+    document_chunks2=qa_model.document_splitter_assistant(documents2,user_input_chunk_size = 400,user_input_chunk_overlap = 30)
                         
 
     # Define the retraiver
-    vectorstore_doc2=qa_model.create_embedding_assistant(document_chunks2)
-
-
+    # vectorstore_doc2=qa_model.create_embedding_assistant(document_chunks2)
     st.write("Comparison : ")
     st.write("\n")
     changes =[]
     changes_1 = []
     highlight_1 = []
     ct = 0
+    # llm = model.generate("what is capital of india?")
+
     for i in document_chunks2:
-        print("*"*100)
-        print("Chunk Counter : ")
+        print("*"*100, "Chunk Counter : ")
         text = i
-        print(i)
-            # QA Model to process the further things with each chunk of text 
+        print(text)
+        st.write("chunkss")
+        st.write(i)
+        query= text
 
-        query= "Find a context that matches the text : {}".format(text)
-        custom_prompt_template ="""Use the following pieces of context to answer the question at the end. Don't try to make up an answer that is not present in the document.	
-        Below is the context provided to you:	
-        {context}	
-        You are an editor of a company who has to provide insight on the changes that were made in the new version of a document compared to the older version and 	
-        explain the changes.	
-        Below is the old version of the document :	
-        {text} 	
-        Compare the details of text containing older version of document with details of context containing the new version of document , and explain the 	
-        changes that were made in the text with reference to context in the below format :	
-        • Change: If there are any difference between the text and context then explain the difference otherwise say 'None'.	
-        	
-        Dont add anything other than the above provided format	
-        Question: {question}	
-        Answer from the given document chunk. The answer for question is """
+        custom_prompt_template ="""
+        You are an experienced legal consultant, your task is identifying the changes made in the latest version of the document as compared to the
+        previous one and mention those changes in the Output.\n
+        The document versions are given below:
+        \n
+        latest version of document:{text}
+        \n
+        previous version of document :{context}
+        \n	
 
+        Guidelines: If there are no substantial legal changes, respond with "No change." Avoid highlighting minor edits such as prepositions, punctuation
+        marks, or formatting adjustments. Focus on meaningful alterations that impact the legal context.
+        Do not make up the things. Give response in short.
+        \n
+        Change: 
+        """
+        
 
-        llm=hf_pipeline
-        custom_prompt_template=custom_prompt_template
-        query=query
+        custom_prompt_template_2 = """
+        In the below provided context there is information to answer the question asked at the end. Read the context properly and answer the question asked to           you at the end :
+        \n Context: \n
+        ##########
+        {context}
+        ########## 
+        Explain the leave policy of Affine?
+        """
+ 
 
-        PROMPT = PromptTemplate(
-                                    template = custom_prompt_template, input_variables = ["context","question"],
-                                    partial_variables={"text": text}
+        llm = hf_pipeline
+        
+
+        PROMPT = PromptTemplate(template = custom_prompt_template, input_variables = ["context"] #,"question"]
+                                     ,partial_variables={"text": query}
                                     )
         chain_type_kwargs = {"prompt": PROMPT}
                     # retriever=chroma_db.as_retriever(type = "similarity", search_kwargs={"k":1})
         custom_qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", 
                                                             retriever=vectorstore_doc1.as_retriever(type = "similarity",search_kwargs={"k":3}),
+                                                            verbose=True,
                                                             chain_type_kwargs=chain_type_kwargs,
                                                             return_source_documents = True)
         
         try:                                                   
             response = custom_qa({"query": query})
-            #st.write(response)
+            st.write("response")
+            st.write(response)
+            # st.write(response['source_documents'])
             result = response['result']
+            # st.write("resulteeeeeeeeeeeee")
+            # st.write(result)
             change = result.split("Change:")[-1]
-
+            # st.write("changeeeeeeeeeeeeeeeeeeeee")
+            # st.write(change)
             if change not in ["None"," None","• Change: None","None.","Change: None","""• Change: None
 
     The provided text matches the context in the document without any differences."""] :
@@ -460,15 +379,22 @@ if trigger_1 :
                 else:
                         highlight_1.extend(j)
                 changes_1.append(change)        
-                ct+=1    
+                ct+=1   
                 st.write(str(ct)+'. '+change)
                 st.write('\n')
                 #st.write('\n')
         except:
             continue
+        if ct==6:
+            break
+        
+
+    
     st.write('\n')
     st.write('\n')
-    path_1= qa_model.pdf_highlight(file_2,highlight_1)
+    st.write("highlight_1")
+    st.write(highlight_1)
+    path_1= qa_model.pdf_highlight1(file_2,highlight_1)
     qa_model.displayPDF(path_1)
     st.write('\n')
     st.write('\n')
